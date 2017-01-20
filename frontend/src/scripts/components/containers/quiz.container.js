3399dd159dbb0c86751ea202050810bb2c80ce37
Anton Burduzha <anton.burduzha@hotmail.com>
2017-01-20 21:14:16 +0200

    added function for checking quantity of true answers;

--- frontend/src/scripts/components/containers/quiz.container.js
+++ frontend/src/scripts/components/containers/quiz.container.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux'
 import {getCurrentQuiz} from '../../api/user.api'
 import {setCurrentQuizData} from '../../actions/quiz.actions'
 import QuizView from '../views/quiz.view'
+import update from 'immutability-helper';
 
 class QuizContainer extends Component {
   constructor() {
     super();
+    this.checkResult = this.checkResult.bind(this);
   }
 
   componentDidMount(){
@@ -16,10 +18,30 @@ class QuizContainer extends Component {
     });
   }
 
+  checkResult(){
+    let clickedAnswers = document.querySelectorAll('.input-check');
+    let verifiedAnswers = [],
+      trueAnswersQuantity = 0,
+      trueAnswersPercent = 0;
+    clickedAnswers.forEach(answer => {
+      if(answer.checked){
+        verifiedAnswers.push(answer.parentNode.textContent);
+      }
+    });
+    for(let i = 0; i < this.props.quizData.currentQuizData.questions.length; i++){
+      if(this.props.quizData.currentQuizData.questions[i].trueAnswer === verifiedAnswers[i]){
+        trueAnswersQuantity++;
+      }
+    }
+    trueAnswersPercent = (trueAnswersQuantity * 100 / this.props.quizData.currentQuizData.questions.length).toFixed(2);
+    //показать на следующей, сделать высоту страниц, подгружать результаты всех тестов, сделать задежку переходов, стили
+  }
+
   render(){
     return (
       <QuizView
-        quizData={this.props.quizData.currentQuizData}/>
+        quizData={this.props.quizData.currentQuizData}
+        checkResult={this.checkResult}/>
     );
   }
 }
--- frontend/src/scripts/components/views/quiz.view.js
+++ frontend/src/scripts/components/views/quiz.view.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router'
-import {Col} from 'react-bootstrap'
+import {Col, Button} from 'react-bootstrap'
 
 export default class QuizView extends Component {
   render() {
@@ -8,8 +8,9 @@ export default class QuizView extends Component {
     quizData = this.props.quizData.questions.map((question, i) => {
       quizAnswers = this.props.quizData.questions[i].answers.map((answer, j) => {
         return <div key={j}>
-          <input type="radio" name={i} id={answer}/>
-          <label htmlFor={answer}>{answer}</label>
+          <label>
+            <input className="input-check" type="radio" name={i} value={answer}/>
+            {answer}</label>
         </div>
       });
       return <li key={i}>
@@ -23,6 +24,7 @@ export default class QuizView extends Component {
       <Col mdOffset={2} md={8} className="article-home">
         <p>{this.props.quizData.name}</p>
         <ol>{quizData}</ol>
+        <Button onClick={this.props.checkResult} className="btn-check-result">Проверить</Button>
       </Col>
     )
   }
