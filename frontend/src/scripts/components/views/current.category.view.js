6a7676f1d4f691ae3f2a96ecd89aa499c0469dd8
Anton Burduzha <anton.burduzha@hotmail.com>
2017-02-15 20:10:38 +0200

    refactored userpage/category component; added react semantic;

--- frontend/src/scripts/components/views/current.category.view.js
+++ frontend/src/scripts/components/views/current.category.view.js
@@ -4,14 +4,21 @@ import {Col} from 'react-bootstrap'
 
 export default class CurrentCategoryView extends Component {
   render() {
-    let quizList;
-    quizList = this.props.quizList.map((item, i) => {
-      return <li key={i}><Link onClick={this.props.getQuizName}>{item}</Link></li>
-    });
     return (
       <Col mdOffset={2} md={8} className="article-main-content">
-        <ol>{quizList}</ol>
+        <CurrentCategoryList getQuizName={this.props.getQuizName} quizList={this.props.quizList} />
       </Col>
     )
   }
 }
+
+const CurrentCategoryListItem = (props) => {
+  return <li><Link onClick={props.getQuizName}>{props.name}</Link></li>
+};
+
+const CurrentCategoryList = (props) => {
+  let quizList = props.quizList.map((item, i) => {
+    return <CurrentCategoryListItem key={i} getQuizName={props.getQuizName} name={item} />
+  });
+  return <ol>{quizList}</ol>
+};
--- frontend/src/scripts/components/views/user.page.view.js
+++ frontend/src/scripts/components/views/user.page.view.js
@@ -3,32 +3,42 @@ import {Col, Image} from 'react-bootstrap'
 
 export default class UserPageView extends Component {
   render() {
-    let completedQuizes = this.props.completedQuizes.map((item, i) => {
-      return <li key={i}>{item.quizName} - {item.result}%</li>
-    });
     return (
       <Col mdOffset={2} md={8} className="article-main-content">
-        <div>
-          <h2 className="text-category text-center">Categories</h2>
-          <div className="article-categories">
-            <div
-              className="article-category-current"
-              onClick={this.props.goToCurrentCategory}>
-              <h3 className="text-category-current text-center">История</h3>
-              <Image className="img-category" src='http://imgdepo.com/id/i10170419' responsive />
-            </div>
-            <div className="article-category-current"
-                 onClick={this.props.goToCurrentCategory}>
-              <h3 className="text-category-current text-center">Математика</h3>
-              <Image className="img-category" src='http://imgdepo.com/id/i10170418' responsive />
-            </div>
-          </div>
+        <h2 className="text-category text-center">Categories</h2>
+        <div className="article-categories">
+          <CategoryItem
+            goToCurrentCategory={this.props.goToCurrentCategory}
+            theme="История"
+            src="http://imgdepo.com/id/i10170419"/>
+          <CategoryItem
+            goToCurrentCategory={this.props.goToCurrentCategory}
+            theme="Математика"
+            src="http://imgdepo.com/id/i10170418"/>
         </div>
-        <div>
-          <h2 className="text-category text-center">Results</h2>
-          <ol>{completedQuizes}</ol>
-        </div>
+        <h2 className="text-category text-center">Results</h2>
+        <CompletedQuizesList completedQuizes={this.props.completedQuizes}/>
       </Col>
     )
   }
 }
+
+const CompletedQuizesListItem = (props) => {
+  return <li>{props.quizName} - {props.result}%</li>
+};
+
+const CompletedQuizesList = (props) => {
+  let completedQuizes = props.completedQuizes.map((item, i) => {
+    return <CompletedQuizesListItem key={i} quizName={item.quizName} result={item.result}/>
+  });
+  return <ol>{completedQuizes}</ol>
+};
+
+const CategoryItem = (props) => {
+  return (
+    <div className="article-category-current" onClick={props.goToCurrentCategory}>
+      <h3 className="text-category-current text-center">{props.theme}</h3>
+      <Image className="img-category" src={props.src} responsive />
+    </div>
+  );
+};
